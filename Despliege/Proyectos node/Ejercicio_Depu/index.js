641bf52e812d862c167384731e8c58ba5127c77f
VicenteCuenca <31209679+VicenteCuenca@users.noreply.github.com>
2018-09-21 16:49:34 +0200

    Ejercicio de depuracion.

---
+++ Despliege/Proyectos node/Ejercicio_Depu/index.js
@@ -0,0 +1,8 @@
+var m = 1, n = 2;
+
+for (var i = 1; i <= 5; i++) {
+    m = m*i;
+    n=n+m*n;  
+}
+
+console.log(n);
--- Despliege/Proyectos node/PruebaNPN/index.js
+++ Despliege/Proyectos node/PruebaNPN/index.js
@@ -1,2 +1,2 @@
 const lodash = require('lodash');
-console.log(lodash.join( ["Alex", "Arturo", "Pablo", "Nacho"],","));
+console.log(lodash.join( ["Alex", "Arturo", "Pablo", "Nacho","Vicente"],","));
